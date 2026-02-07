66af9006fc7e3bfc434c30dcc91325d20d8daeef
Eternal_Recursion <2945382316@qq.com>
2026-02-07 16:43:54 +0800

    Update thread_pool.cpp

--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -4,13 +4,13 @@ using namespace std;
 // 构造函数，传入线程数
 thread_pool::thread_pool(int thread_numbers) : stop(false) {
         while(thread_numbers--){
-	// 以lambda表达式初始化线程，lambda表达式内部循环从任务队列中取出任务函数
-                threads.emplace_back([this](){
+				// 以lambda表达式初始化线程，lambda表达式内部循环从任务队列中取出任务函数
+        		threads.emplace_back([this](){
 
                         while(true){
                                 std::function<void()> func;
                                 {
-		        // 若队列为空，则条件变量调用wait等待，出现任务，则notify之后开始执行，或者stop之后队列为空就退出循环
+		        						// 若队列为空，则条件变量调用wait等待，出现任务，则notify之后开始执行，或者stop之后队列为空就退出循环
                                         std::unique_lock<std::mutex> lock(mtx);
                                         cv.wait(lock, [this](){
                                                  return stop || !tasks.empty();
